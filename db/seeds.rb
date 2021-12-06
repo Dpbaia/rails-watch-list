94f5316a7fc1e4cc53d78780aa88a9638bf4a855
Diana Baiadori <diana.baiadori@gmail.com>
2021-12-06 17:54:18 +0100

    fixing up the watchlist: using API to make the seeds, adjusting CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -11,16 +13,36 @@
 # Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 # Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-p 'Creating new movies...'
-20.times do
-  title = Faker::Movie.title
-  Movie.create(
-    title: title,
-    overview: Faker::GreekPhilosophers.quote,
-    poster_url: "https://source.unsplash.com/1600x900/?#{title}",
-    rating: (1..10).to_a.sample
+p "Destroying the database"
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated?page=3'
+movie_serialised = URI.open(url).read
+movie_j = JSON.parse(movie_serialised)
+
+movies = movie_j['results']
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
   )
-  p "Created #{title}"
+  p "Created #{movie['title']}"
 end
+
+
+# p 'Creating new movies...'
+# 20.times do
+#   title = Faker::Movie.title
+#   Movie.create(
+#     title: title,
+#     overview: Faker::GreekPhilosophers.quote,
+#     poster_url: "https://source.unsplash.com/1600x900/?#{title}",
+#     rating: (1..10).to_a.sample
+#   )
+#   p "Created #{title}"
+# end
 
 p 'Finished seeding'
